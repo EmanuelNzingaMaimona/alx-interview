1a5d3d007d8a5004165a52889a1db8ff154a4535
Emanuel Nzinga Maimona <emanuelmaimona@hotmail.com>
2021-07-21 23:26:57 +0100

    add task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" checking if all boxes in the list of list can be opened """
+""" check if all boxes in the list of list can be opened """
 
 
 def canUnlockAll(boxes):
-    """ determine if all boxes can be opened """
+    """ determining if all boxes can be opened """
     for key in range(1, len(boxes) - 1):
         res = False
         for index in range(len(boxes)):
