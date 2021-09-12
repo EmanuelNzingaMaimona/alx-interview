e3fccb231b4a66e80361a1032ad626ad5a84661b
Emanuel Nzinga Maimona <EmanuelNzingaMaimona>
2021-09-13 00:28:44 +0200

    add task

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' A program to determine the fewest number of coins needed to meet a given amount total '''
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    change = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
+        if total == 0:
+            return change
+    if total != 0:
+        return -1
+
